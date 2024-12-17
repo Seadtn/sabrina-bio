9eb91f1077826d75a6734648e9ce50d13da98540
Seadtn <nasraminefsg@gmail.com>
2024-12-17 22:11:43 +0100

    fix: addImage

--- src/components/Admin/product/ProductTable.js
+++ src/components/Admin/product/ProductTable.js
@@ -1,14 +1,24 @@
-import React from 'react';
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, IconButton } from '@mui/material';
-import EditIcon from '@mui/icons-material/Edit';
-import DeleteIcon from '@mui/icons-material/Delete';
+import React from "react";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  IconButton,
+} from "@mui/material";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
 
-const ProductTable = ({ products, onEdit,onView, onDelete }) => {
+const ProductTable = ({ products, onEdit, onView, onDelete }) => {
   return (
     <TableContainer component={Paper}>
       <Table>
         <TableHead>
           <TableRow>
+            <TableCell>Image</TableCell>
             <TableCell>Name</TableCell>
             <TableCell>Description</TableCell>
             <TableCell>Price</TableCell>
@@ -20,15 +30,35 @@ const ProductTable = ({ products, onEdit,onView, onDelete }) => {
         </TableHead>
         <TableBody>
           {products.map((product) => (
-            <TableRow key={product.id} onClick={(event) => onView(product,event)}>
+            <TableRow
+              key={product.id}
+              onClick={(event) => onView(product, event)}
+            >
+              <TableCell>
+                {" "}
+                <img
+                  src={`data:image/jpeg;base64,${btoa(
+                    new Uint8Array(product.image).reduce(
+                      (data, byte) => data + String.fromCharCode(byte),
+                      ""
+                    )
+                  )}`}
+                  alt={`product${product.id}`}
+                  style={{ width: "50px", height: "50px", objectFit: "cover" }}
+                />
+              </TableCell>
               <TableCell>{product.name}</TableCell>
               <TableCell>{product.description}</TableCell>
               <TableCell>{product.price}</TableCell>
               <TableCell>{product.quantity}</TableCell>
               <TableCell>{product.creationDate}</TableCell>
-              <TableCell>{product.inSold === true && product.soldRation > 0 ? `Yes (${product.soldRation}%)`:'No'}</TableCell>
               <TableCell>
-                <IconButton 
+                {product.inSold === true && product.soldRation > 0
+                  ? `Yes (${product.soldRatio}%)`
+                  : "No"}
+              </TableCell>
+              <TableCell>
+                <IconButton
                   onClick={(event) => {
                     event.stopPropagation();
                     onEdit(product);
